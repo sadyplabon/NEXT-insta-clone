8f73ff7133b46066ff04e8e5a36bd982bed55ce4
sadyplabon <plabon.pen@gmail.com>
2024-02-07 20:21:56 +0600

    create post header and image component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,7 +1,22 @@
+import { HiDotsHorizontal } from "react-icons/hi";
+
 export default function Post({ img, userImg, caption, username, id }) {
   return (
-    <div>
-      <h1>{username}</h1>
+    <div className="bg-white my-7 border rounded-md">
+      {/* Post Header */}
+      <div className="flex items-center p-5">
+        <img
+          className="h-12 rounded-full object-cover border p-1 mr-3"
+          src={userImg}
+          alt={username}
+        />
+        <p className="font-bold flex-1">{username}</p>
+        <HiDotsHorizontal className="h-5" />
+      </div>
+      {/* Post Image */}
+      <div>
+        <img className="object-cover w-full" src={img} alt={id} />
+      </div>
     </div>
   );
 }
