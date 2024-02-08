a7f6ffdae922fd52482d8604e85c2967cff8bfe6
sadyplabon <plabon.pen@gmail.com>
2024-02-08 14:18:27 +0600

    create mini profile section

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,3 +1,4 @@
+import MiniProfile from "./MiniProfile";
 import Posts from "./Posts";
 import Stories from "./Stories";
 
@@ -13,9 +14,11 @@ export default function Feed() {
       </section>
 
       <section className="hidden md:inline-grid md:col-span-1">
-        {/* Mini Profile */}
-
-        {/* Suggections */}
+        <div className="fixed w-[380px]">
+          {/* Mini Profile */}
+          <MiniProfile />
+          {/* Suggestions */}
+        </div>
       </section>
     </main>
   );
