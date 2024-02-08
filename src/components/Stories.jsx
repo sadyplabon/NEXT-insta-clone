35c50fd639f0c7dee5a4d44ab064800381459b3a
sadyplabon <plabon.pen@gmail.com>
2024-02-08 14:51:35 +0600

    add suggestion component

--- src/components/Stories.jsx
+++ src/components/Stories.jsx
@@ -13,7 +13,6 @@ export default function Stories() {
       id: i,
     }));
     setSoryUsers(storyUsers);
-    console.log(storyUsers);
   }, []);
   return (
     <div className="flex space-x-2 p-6 bg-white mt-8 border-gray-200 border overflow-x-scroll rounded-sm scrollbar-none">
