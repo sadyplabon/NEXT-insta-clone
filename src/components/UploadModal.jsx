5113aaee605183d94b8a412fd4e92673d747a962
sadyplabon <plabon.pen@gmail.com>
2024-02-09 15:10:11 +0600

    create post and upload file to firestore

--- src/components/UploadModal.jsx
+++ src/components/UploadModal.jsx
@@ -1,14 +1,47 @@
-"use client";
 import { useRecoilState } from "recoil";
 import { modalState } from "../../atom/modalAtom";
 import Modal from "react-modal";
 import { MdOutlinePhotoCamera } from "react-icons/md";
 import { useRef, useState } from "react";
+import {
+  addDoc,
+  collection,
+  serverTimestamp,
+  updateDoc,
+} from "firebase/firestore";
+import { db, storage } from "../../firebase";
+import { useSession } from "next-auth/react";
+import { getDownloadURL, ref, uploadString } from "firebase/storage";
 
 export default function UploadModal() {
   const [open, setOpen] = useRecoilState(modalState);
   const [selectedFile, setSelectedFile] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const { data: session } = useSession();
   const filePickerRef = useRef(null);
+  const captionRef = useRef(null);
+  async function uploadPost() {
+    if (loading) return;
+    setLoading(true);
+    const docRef = await addDoc(collection(db, "posts"), {
+      caption: captionRef.current.value,
+      username: session.user.username,
+      profileImg: session.user.image,
+      timestamp: serverTimestamp(),
+    });
+    const imageRef = ref(storage, `posts/${docRef.id}/image`);
+    await uploadString(imageRef, selectedFile, "data_url").then(
+      async (snapshot) => {
+        const downloadURL = await getDownloadURL(imageRef);
+        await updateDoc(doc(db, "posts", docRef.id), {
+          image: downloadURL,
+        });
+      }
+    );
+    setOpen(false);
+    setLoading(false);
+    setSelectedFile(null);
+  }
   function addImageToPost(e) {
     const reader = new FileReader();
     if (e.target.files[0]) {
@@ -55,9 +88,11 @@ export default function UploadModal() {
               maxLength={150}
               placeholder="Add caption..."
               className="m-4 border-none text-center focus:ring-0 w-full"
+              ref={captionRef}
             />
             <button
-              disabled
+              disabled={!selectedFile || loading}
+              onClick={uploadPost}
               className="w-full bg-red-600 rounded text-white p-2 shadow-md hover:brightness-125 disabled:bg-gray-200 disabled:cursor-not-allowed disabled:hover:brightness-100"
             >
               Upload Post
